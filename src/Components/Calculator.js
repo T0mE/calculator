6a2851a83507a5bd156071af2b3c27046c6aab2b
Tomek <qomekp@gmail.com>
2023-05-18 21:28:00 +0200

    add input

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,7 +1,36 @@
+import '../Styles/Calculator.css';
+
 function Calculator() {
 	return (
-		<div>
-			<p>lorem</p>
+		<div className='container'>
+			<h1>Mortgage calculator</h1>
+			<div className='d-flex flex-row'>
+				<div className=''>
+					<label className='mb-1'>Home price</label>
+					<input
+						type='number'
+						class='form-control'
+						placeholder='$1000000'
+					></input>
+				</div>
+				<div className=''>
+					<label className='mb-1'>Down payment(6%)</label>
+					<input
+						type='tel'
+						class='form-control'
+						placeholder='$60,000'
+						inputMode='numeric'
+					></input>
+				</div>
+				<div className='d-flex align-items-end'>
+					<input
+						type='tel'
+						class='form-control'
+						placeholder='3%'
+						inputMode='numeric'
+					></input>
+				</div>
+			</div>
 		</div>
 	);
 }
